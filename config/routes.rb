de17eea32eb1da7065e41f2ed6c3a7d922f039da
tanaken <tanaka.kentaro@pepabo.com>
2018-12-04 10:08:52 +0900

    use resources method: index, show, new, create, edit, destroy に関するルーティングを一括で設定してくれるくん

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 end
