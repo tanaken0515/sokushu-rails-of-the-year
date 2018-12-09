2f306d8f8e6e3c4cff71729d7226218066490586
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 13:48:47 +0900

    fix: ここはインスタンス変数じゃなきゃダメなのよ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
