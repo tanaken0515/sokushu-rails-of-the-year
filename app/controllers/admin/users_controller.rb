393f0d79dd1382ea0e0ec3ad8c732000e6dbb076
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 17:03:41 +0900

    user edit and update action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,13 +7,24 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def show
