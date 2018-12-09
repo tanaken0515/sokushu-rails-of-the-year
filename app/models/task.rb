58a66c8f299f215ffe6efa1dbdac0120e958369b
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 13:41:10 +0900

    privateメソッドの位置を間違えていたのでまず修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,10 @@ class Task < ApplicationRecord
             presence: true,
             length: {maximum: 30}
   validate :validate_name_not_including_comma
-end
 
-private
+  private
 
-def validate_name_not_including_comma
-  errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
