941df65cd57a7bd8926433767fad5be711a3dc12
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 15:54:08 +0900

    users create action and user_params

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,16 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render.new
+    end
+  end
+
   def edit
   end
 
@@ -10,5 +20,11 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
