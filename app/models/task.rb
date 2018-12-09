4c19e8f1164240b41e30dcc50c0c61baf348ec5f
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 11:50:06 +0900

    add original validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ApplicationRecord
   validates :name,
             presence: true,
             length: {maximum: 30}
+  validate :validate_name_not_including_comma
+end
+
+private
+
+def validate_name_not_including_comma
+  errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
 end
