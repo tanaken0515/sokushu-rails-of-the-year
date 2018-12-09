ee3ed64f0e44fc5b8bf8ca9238859f8856c9aa38
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-09 18:35:29 +0900

    ログイン情報を取得しやす来るため helper_method を利用して ApplicationController内に実装する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||=  User.find(id: session[:user_id]) if session[:user_id]
+  end
 end
