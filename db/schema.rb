a6fdfacfcce7a7316a9318cf2b8f6d58701ff8a9
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-10 23:43:04 +0900

    NULL禁止の制約を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_140644) do
+ActiveRecord::Schema.define(version: 2018_12_10_143125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_140644) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
