185bd96ae8b8aad4bed516f05d002522b5d3a7da
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-11 00:27:14 +0900

    use scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,7 @@ class Task < ApplicationRecord
             length: {maximum: 30}
   validate :validate_name_not_including_comma
 
+  scope :recent, -> { order(created_at: :desc) }
   private
 
   def validate_name_not_including_comma
