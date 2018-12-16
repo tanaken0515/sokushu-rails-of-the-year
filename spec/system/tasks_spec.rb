b5987ccc3e4d4f1106172296149d4671ea823c79
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-16 16:52:47 +0900

    tasks_spec outline

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作る
+      # ユーザーAのタスクを作る
+    end
+
+    context 'ユーザーAがログインしているとき' do
+      before do
+        # login
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 確認
+      end
+    end
+  end
+end
