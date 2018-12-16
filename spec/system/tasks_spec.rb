d27a6c8edc3dd8136f0121c8b42a52f7059faa57
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-16 16:57:31 +0900

    create user and task

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     before do
-      # ユーザーAを作る
-      # ユーザーAのタスクを作る
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
     end
 
     context 'ユーザーAがログインしているとき' do
