7353aef96b1ecb66f0e6a0ce2259f883275aa87b
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2018-12-16 16:59:54 +0900

    add login operation

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -9,7 +9,10 @@ describe 'タスク管理機能', type: :system do
 
     context 'ユーザーAがログインしているとき' do
       before do
-        # login
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
       end
 
       it 'ユーザーAが作成したタスクが表示される' do
