6ca3792b3031c16a241e613d9996da04c1f289e5
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-01-13 17:11:17 +0900

    サーバー側でJSを作ってレスポンスする(SJR)方式に書き換える

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,7 @@
-document.addEventListener('turbolinks:load', function () {
-  document.querySelectorAll('.delete').forEach(function (a) {
-    a.addEventListener('ajax:success', function () {
-      a.parentNode.parentNode.style.display = 'none';
-    });
-  });
-});
+// document.addEventListener('turbolinks:load', function () {
+//   document.querySelectorAll('.delete').forEach(function (a) {
+//     a.addEventListener('ajax:success', function () {
+//       a.parentNode.parentNode.style.display = 'none';
+//     });
+//   });
+// });
