222006a527f174a32b0473686971e4d4217b679c
tanaken0515 <tanaka.kentaro.kk1222@gmail.com>
2019-01-13 16:21:52 +0900

    Ajax通信に成功した時に発行される"ajax:success"イベントをトリガーとして、削除ボタンがクリックされた行を消す

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,7 @@
 document.addEventListener('turbolinks:load', function () {
-  document.querySelectorAll('td').forEach(function (td) {
-    td.addEventListener('mouseover', function (e) {
-      e.currentTarget.style.backgroundColor = '#eff';
-    });
-
-    td.addEventListener('mouseout', function (e) {
-      e.currentTarget.style.backgroundColor = '';
+  document.querySelectorAll('.delete').forEach(function (a) {
+    a.addEventListener('ajax:success', function () {
+      a.parentNode.parentNode.style.display = 'none';
     });
   });
 });
